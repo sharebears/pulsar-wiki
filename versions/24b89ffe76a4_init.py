21cc591539300e48b40cc3c08bf70d0b3acce151
lights <lights@tutanota.de>
2018-10-11 16:28:47 -0400

    add initial db migration

---
+++ versions/24b89ffe76a4_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: 24b89ffe76a4
+Revises: 
+Create Date: 2018-10-11 16:25:57.706042
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '24b89ffe76a4'
+down_revision = None
+branch_labels = ('wiki',)
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
