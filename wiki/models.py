4ebfdc9f0c89c6163418c4d16034233be5cae136
HHughes <throwa915@protonmail.com>
2018-10-16 12:24:06 -0500

    Allow wiki article creation

--- wiki/models.py
+++ wiki/models.py
@@ -4,7 +4,7 @@ from typing import List
 import flask
 from sqlalchemy import func
 
-from core import db
+from core import cache, db
 from core.mixins import SinglePKMixin
 from core.users.models import User
 from core.utils import cached_property
@@ -17,14 +17,37 @@ class WikiArticle(db.Model, SinglePKMixin):
     __tablename__ = 'wiki_articles'
     __serializer__ = WikiArticleSerializer
     __cache_key__ = 'wiki_articles_{id}'
+    __cache_key_all__ = 'wiki_articles_all'
 
     id: int = db.Column(db.Integer, primary_key=True)
     title: str = db.Column(db.String(128), nullable=False)
+    contents: str = db.Column(db.Text, nullable=False)
     last_editor_id: int = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     last_updated: datetime = db.Column(
         db.DateTime(timezone=True), nullable=False, server_default=func.now())
-    contents: str = db.Column(db.Text, nullable=False)
     revision: int = db.Column(db.Integer, nullable=False)
+    deleted: bool = db.Column(db.Boolean, nullable=False, server_default='f', index=True)
+
+    @classmethod
+    def get_all(cls, include_dead: bool = False) -> List['WikiArticle']:
+        return cls.get_many(
+                key=cls.__cache_key_all,
+                include_dead=include_dead,
+                required_properties=('last_editor', ))
+
+    @classmethod
+    def new(cls,
+            title: str,
+            contents: str,
+            last_editor_id: int,
+            revision: int) -> 'WikiArticle':
+        User.is_valid(last_editor_id, error=True)
+        cache.delete(cls.__cache_key_all__.format(id=id))
+        return super()._new(
+            title=title,
+            contents=contents,
+            revision=revision,
+            last_editor_id=last_editor_id)
 
     @cached_property
     def last_editor(self):
