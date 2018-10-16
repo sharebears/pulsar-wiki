a8ac3a974d130f6cf65611a527765a6ba0742892
HHughes <throwa915@protonmail.com>
2018-10-15 22:09:37 -0500

    Flesh out data in classes

--- wiki/models.py
+++ wiki/models.py
@@ -16,18 +16,46 @@ app = flask.current_app
 class WikiArticle(db.Model, SinglePKMixin):
     __tablename__ = 'wiki_articles'
     __serializer__ = WikiArticleSerializer
-    __cache_key__ = 'wiki_articles_{id}'
+    __cache_key__ = 'wiki_article_{id}'
 
     _articles = List['WikiArticle']
 
     id: int = db.Column(db.Integer, primary_key=True)
     title: str = db.Column(db.String(128), nullable=False)
-    last_editor_id: int = db.Column(db.Integer, db.ForeighKey('users.id'), nullable=False)
+    last_editor_id: int = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     last_updated: datetime = db.Column(
         db.DateTime(timezone=True), nullable=False, server_default=func.now())
     contents: str = db.Column(db.Text, nullable=False)
-    version: int = db.Column(db.Integer, nullable=False)
+    revision: int = db.Column(db.Integer, nullable=False)
 
     @cached_property
     def last_editor(self):
         return User.from_pk(self.last_editor_id)
+
+
+class WikiArticleRevision(db.Model, SinglePKMixin):
+    __tablename__ = 'wiki_article_revisions'
+    __serializer__ = WikiRevisionSerializer
+    __cache_key__ 'wiki_revision_{id}'
+
+    _revisions = List['WikiArticleRevision']
+
+    id: int = db.Column(db.Integer, primary_key=True)
+    wiki_id: int = db.Column(db.Integer, db.ForeignKey('wiki_articles.id'), nullable=False)
+    title: str = db.Column(db.String(128), nullable=False)
+    editior_id: int = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    time_created: datetime = db.Column(
+        db.DateTime(timezone=True), nullable=False, server_default=func.now())
+    contents: str = db.Column(db.Text, nullable=False)
+    revision: int = db.Column(db.Integer, nullable=False)
+
+
+class WikiArticleAliases(db.Model, SinglePKMixin):
+    __tablename__ = 'wiki_article_aliases'
+    __cache_key__ = 'wiki_article_alias_{id}'
+
+    _aliases = List['WikiArticleRevision']
+
+    id: int = db.Column(db.Integer, primary_key=True)
+    wiki_id: int = db.Column(db.Integer, db.ForeignKey('wiki_articles.id'), nulla      ble=False)
+    alias: str = db.Column(db.String(128), nullable=False)
--- wiki/serializers.py
+++ wiki/serializers.py
@@ -5,7 +5,16 @@ class WikiArticleSerializer(Serializer):
     id = Attribute()
     title = Attribute()
     contents = Attribute()
-    version = Attribute()
+    revision = Attribute()
     last_updated = Attribute()
-    last_editor = Attribute(nested=('id', 'username', ))
-    deleted = Attribute(permission='modify_wiki')
+    last_editor_id = Attribute(nested=('id', 'username', ))
+
+
+class WikiArticleRevision(Serializer):
+    id = Attribute()
+    wiki_id = Attribute()
+    title = Attribute()
+    editor_id = Attribute(nested=('id', 'username', ))
+    time_created = Attribute()
+    contents = Attribute()
+    revision = Attribute()
