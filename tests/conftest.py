9d988f0f498e1b01d34e48c84512ade1a6653f38
lights <lights@tutanota.de>
2018-10-17 18:57:47 -0400

    remove population functions from wiki conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,6 @@
-import pytest
-
 import wiki
-from core import db
 from core.conftest import *  # noqa: F401, F403
-from core.conftest import PLUGINS, UNPOPULATE_FUNCTIONS
-
-
-@pytest.fixture(autouse=True)
-def populate_db_(app, client):
-    pass
-
-
-def unpopulate_database():
-    db.engine.execute("DELETE FROM some_table")
+from core.conftest import PLUGINS
 
 
 PLUGINS.append(wiki)
-UNPOPULATE_FUNCTIONS.append(unpopulate_database)
