a16aac2edfa56a1a274edf6d5d665c5de130fbcb
HHughes <throwa915@protonmail.com>
2018-10-20 15:01:44 -0500

    fix some issues

--- wiki/routes/article.py
+++ wiki/routes/article.py
@@ -15,11 +15,9 @@ VIEW_WIKI_SCHEMA = Schema({
 
 @bp.route('/wiki/<int:id>', methods=['GET'])
 @require_permission('wiki_view')
-@validate_data('VIEW_WIKI_SCHEMA')
-def view_wiki(id: int,
-              language: str) -> flask.Response:
-    wiki = Wiki.from_pk(
-        id,
-        _404=True,
-        include_dead=flask.g.user.has_permission('wiki_modify_addvanced'))
-    return flask.jsonify(wiki)
+@validate_data(VIEW_WIKI_SCHEMA)
+def view_wiki(id: int, language: str) -> flask.Response:
+    return flask.jsonify(Wiki.from_pk(
+                                    id,
+                                    _404=True,
+                                    include_dead=flask.g.user.has_permission('wiki_modify_advanced')))
